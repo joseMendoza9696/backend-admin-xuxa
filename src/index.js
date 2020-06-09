0488e2493ae13d440a3ee1cef19beff5b32a41a3
joseMendoza9696 <jmendozacarrasco@gmail.com>
2020-06-09 15:03:07 -0400

    agregando el dashboard

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ require('./db/mongoose');
 const empleadoRouter = require('./routes/empleado');
 const adminRouter = require('./routes/admin');
 
-const cors = require('cors');
+// const cors = require('cors');
 const moment = require('moment');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 // permite que podamos hacer peticiones desde otro server. siempre debe estar antes de las rutas
-app.use(cors({ origin: 'http://localhost:4200' }));
+// app.use(cors({ origin: 'http://localhost:4200' }));
+
+app.use( express.static('dashboard-frontend') );
 
 app.use(empleadoRouter);
 app.use(adminRouter);
