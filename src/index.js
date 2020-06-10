74c2066e7003b8f2bf2ee9825d86e7495e2778c0
joseMendoza9696 <jmendozacarrasco@gmail.com>
2020-06-09 22:24:06 -0400

    Corrigiendo errores del CORS

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ require('./db/mongoose');
 const empleadoRouter = require('./routes/empleado');
 const adminRouter = require('./routes/admin');
 
-// const cors = require('cors');
+const cors = require('cors');
 const moment = require('moment');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 // permite que podamos hacer peticiones desde otro server. siempre debe estar antes de las rutas
-// app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(cors({ origin: 'http://localhost:4200' }));
 
 app.use( '/admin', express.static('dist/dashboard-frontend'));
 app.use( '/empl', express.static('dist/empleado-frontend'));
