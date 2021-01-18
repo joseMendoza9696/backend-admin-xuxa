d03ffa3a926991624c15bbebe76db41702336eef
joseMendoza9696 <jmendozacarrasco@gmail.com>
2021-01-18 15:59:32 -0400

    Solo para la TESIS

--- src/controllers/empleado.controller.js
+++ src/controllers/empleado.controller.js
@@ -26,7 +26,57 @@ logout = async (req, res) => {
 }
 
 
+// esto solo es para la tesis hay que eliminarlo despues o colocarlo en un repositorio
+tesisWeb = async (req, res) => {
+    try {
+        res.status(200).send( 'Tu eres MR.ROBOTS - Que estas haciendo aqui? ' )
+    } catch(error) {
+        res.status(400).send(error)
+    }
+}
+
+robots = async (req, res) => {
+    try {
+        res.status(200).send({ 'User-agent': '*', 'Disallow': '/59gt1' })
+    } catch (error){
+        res.status(400).send(error)
+    }
+}
+
+flagRobots = async (req, res) => {
+    try {
+        res.status(200).send({ 'Flag': 'UCB{el_senor_robot}' })
+    } catch (error){
+        res.status(400).send(error)
+    }
+}
+
+webDELETE = async (req, res) => {
+    try {
+        if ( req.method === 'GET' ) {
+            // Creo que tu peticion es para eliminar algo!!, aqui no encontraras nada
+            res.status(200)
+                .send('Q3JlbyBxdWUgdHUgcGV0aWNpb24gZXMgcGFyYSBlbGltaW5hciBhbGdvISEsIGFxdWkgbm8gZW5jb250cmFyYXMgbmFkYQ==')
+        } else if( req.method === 'DELETE' ){
+            res.header('flag', 'UCB{always_watch_headers}')
+            // Tu flag esta en las cabeceras de esta peticion
+            res.status(200)
+                .send('VHUgZmxhZyBlc3RhIGVuIGxhcyBjYWJlY2VyYXMgZGUgZXN0YSBwZXRpY2lvbg==')
+        } else {
+            // Aqui no esta lo que estas buscando
+            res.status(200).send('QXF1aSBubyBlc3RhIGxvIHF1ZSBlc3RhcyBidXNjYW5kbw==')
+        }
+    } catch (error){
+        res.status(400).send(error)
+    }
+}
+
 module.exports = {
     login,
     logout,
+    // es para la tesis
+    tesisWeb,
+    robots,
+    flagRobots,
+    webDELETE
 }
--- src/routes/empleado.js
+++ src/routes/empleado.js
@@ -3,12 +3,25 @@ const router = new express.Router();
 const moment = require('moment');
 moment.locale('es');
 const empleadoAuth = require('../middleware/empleadoAuth');
-const { login, logout } = require('../controllers/empleado.controller');
+const { login, logout,
+    tesisWeb, robots, flagRobots, webDELETE
+} = require('../controllers/empleado.controller');
 
 
 router.post('/emp/login', login );
 
 router.post('/emp/logout', empleadoAuth, logout );
+
+// esta parte se va a eliminar solo es para la tesis
+router.get('/tesis/web-robots', tesisWeb)
+router.get('/robots.txt', robots)
+router.get('/59gt1', flagRobots)
+
+router.get('/tesis/web-put', webDELETE)
+router.patch('/tesis/web-put', webDELETE)
+router.put('/tesis/web-put', webDELETE)
+router.post('/tesis/web-put', webDELETE)
+router.delete('/tesis/web-put', webDELETE)
 
 module.exports = router;
 
